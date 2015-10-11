5e02451bd05d1fc66eeda9d708a5e5917b040f5b
anca <stefan.7@osu.edu>
2015-10-11 14:25:17 -0400

    Project 1 - Updated 3 and 4

--- plot3.R
+++ plot3.R
@@ -13,6 +13,6 @@ with(d,plot(d$Time,d$Sub_metering_1,type = 'n',xlab = "",ylab = "Energy sub-mete
 points(d$Time,d$Sub_metering_1,type='l')
 points(d$Time,d$Sub_metering_2,type='l',col="red")
 points(d$Time,d$Sub_metering_3,type='l',col="blue")
-legend("topright",lty=1,col=c("black","blue","red"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 dev.off()
--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@ with(d, {
     points(d$Time,d$Sub_metering_1,type='l')
     points(d$Time,d$Sub_metering_2,type='l',col="red")
     points(d$Time,d$Sub_metering_3,type='l',col="blue")
-    legend("topright",lty=1,col=c("black","blue","red"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+    legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
     with(d,plot(d$Time,Global_reactive_power,type = 'l',xlab = "datetime"))
 })
 
